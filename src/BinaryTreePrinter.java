002cdf5c2e34e1767d2f23badc58cc2619e26118
PeshkovDmitry <peshkov-dv@yandex.ru>
2023-08-06 00:40:39 +0300

    Проделана часть работы по выводу дерева на экран

--- src/BinaryTreePrinter.java
+++ src/BinaryTreePrinter.java
@@ -1,23 +1,118 @@
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 public class BinaryTreePrinter<T extends Comparable<T>> implements Printer{
 
-    private List<T> root;
+
 
     public BinaryTreePrinter(Tree tree) {
+        List<Pair> list = getFullTreeAsList(tree);
 
-
+        System.out.println(list);
     }
+
+    /**
+     * Метод, возвращающий дерево в виде последовательного списка
+     * Последовательность повторяет просмотр по ширине
+     * При этом отсутствующие узлы также добавляются в виде пустого
+     * экземпляра класса Pair
+     * @param tree
+     * @return
+     */
+
+    private List<Pair> getFullTreeAsList(Tree tree) {
+        List<Pair> list = new ArrayList<>();
+        Deque<Node<T>> deque = new LinkedList<>();
+        deque.add(tree.getRoot());
+        int valueCount = 1;
+        boolean run = true;
+        int depth = depth(tree.getRoot(),0);
+        while (deque.size() > 0 && run) {
+            Node<T> currentNode = deque.pollFirst();
+            Pair pair = new Pair();
+            if (currentNode != null) {
+                valueCount++;
+                pair = new Pair(currentNode.getValue().toString(), currentNode.getColor());
+                deque.add(currentNode.getLeftChild());
+                deque.add(currentNode.getRightChild());
+            }
+            else {
+                deque.add(null);
+                deque.add(null);
+            }
+            list.add(pair);
+            if (Math.pow(2, depth) == list.size() + 1)
+                run = false;
+        }
+        return list;
+    }
+
+    /**
+     * Метод для получения глубины дерева
+     * @param node
+     * @param currentDepth
+     * @return
+     */
+    private int depth(Node<T> node, int currentDepth) {
+        if (node == null)
+            return currentDepth;
+        if (node.getLeftChild() == null && node.getRightChild() == null)
+            return currentDepth + 1;
+        return currentDepth + 1 +
+                Math.max(
+                    depth(node.getLeftChild(), currentDepth),
+                    depth(node.getRightChild(), currentDepth)
+                );
+    }
+
     @Override
     public void print(Tree tree) {
 
     }
 
-    private Node<T> createFullTree(Tree tree) {
+    private class Pair {
 
+        private String text;
 
-        return null;
+        private Color color;
+
+        public Pair() {}
+
+        public Pair(String text, Color color) {
+            this.text = text;
+            this.color = color;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public Color getColor() {
+            return color;
+        }
+
+        public boolean isEmpty() {
+            return text == null && color == null;
+        }
+
+        @Override
+        public String toString() {
+            String out = "";
+            if (color == Color.RED)
+                out = ANSI_RED + text + ANSI_RESET;
+            else if (color == Color.BLACK)
+                out = ANSI_BLACK + text + ANSI_RESET;
+            else
+                out = text;
+            return out;
+        }
+
+        private String ANSI_RESET = "\u001B[0m";
+        private String ANSI_BLACK = "\u001B[30m";
+        private String ANSI_RED = "\u001B[31m";
+
     }
 
-//    private List<List<String>> get
 }
--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,21 @@ public class Main {
 
         BinaryTree<Integer> myTree = new BinaryTree<>();
 
-        for (int i = 0; i < 5; i++) {
-            myTree.add(new Random().nextInt(100));
-        }
+//        for (int i = 0; i < 5; i++) {
+//            myTree.add(new Random().nextInt(100));
+//        }
 
 
-//        myTree.add(5);
-//        myTree.add(9);
-//        myTree.add(8);
-//        myTree.add(7);
-//        myTree.add(6);
+        myTree.add(5);
+        myTree.add(9);
+        myTree.add(8);
+        myTree.add(7);
+        myTree.add(6);
 
 
-        myTree.printTree();
+
+//        myTree.printTree();
+        BinaryTreePrinter printer = new BinaryTreePrinter(myTree);
 
 
 
