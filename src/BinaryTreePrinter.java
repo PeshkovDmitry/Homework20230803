461e662455f476501149f83c472cfd631d425692
PeshkovDmitry <peshkov-dv@yandex.ru>
2023-08-06 01:50:27 +0300

    Сделан наглядный вывод дерева на экран

--- src/BinaryTreePrinter.java
+++ src/BinaryTreePrinter.java
@@ -1,50 +1,63 @@
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class BinaryTreePrinter<T extends Comparable<T>> implements Printer{
 
-
-
-    public BinaryTreePrinter(Tree tree) {
-        List<Pair> list = getFullTreeAsList(tree);
-
-        System.out.println(list);
+    @Override
+    public void print(Tree tree) {
+        List<String> list = getFullTreeAsList(tree);
+        int maxValueLength = 0;
+        for (String s: list) {
+            if (s.replace(ANSI_RED,"").replace(ANSI_RESET,"").length() > maxValueLength) {
+                maxValueLength = s.replace(ANSI_RED,"").replace(ANSI_RESET,"").length();
+            }
+        }
+        maxValueLength += 2;
+        int level = 0;
+        int nextNewLine = 0;
+        int depth = depth(tree.getRoot(),0);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(getFormattedString(list.get(i),(int) (maxValueLength * Math.pow(2, depth - level - 1))));
+            if (i == nextNewLine) {
+                System.out.println();
+                level++;
+                nextNewLine += Math.pow(2, level);
+            }
+        }
     }
 
     /**
-     * Метод, возвращающий дерево в виде последовательного списка
-     * Последовательность повторяет просмотр по ширине
-     * При этом отсутствующие узлы также добавляются в виде пустого
-     * экземпляра класса Pair
+     * Метод, возвращающий дерево в виде последовательного списка значений узлов.
+     * Последовательность повторяет просмотр по ширине.
+     * При этом отсутствующие узлы также добавляются в виде пустой строки.
      * @param tree
      * @return
      */
 
-    private List<Pair> getFullTreeAsList(Tree tree) {
-        List<Pair> list = new ArrayList<>();
+    private List<String> getFullTreeAsList(Tree tree) {
+        List<String> list = new ArrayList<>();
         Deque<Node<T>> deque = new LinkedList<>();
         deque.add(tree.getRoot());
-        int valueCount = 1;
-        boolean run = true;
         int depth = depth(tree.getRoot(),0);
+        boolean run = true;
         while (deque.size() > 0 && run) {
             Node<T> currentNode = deque.pollFirst();
-            Pair pair = new Pair();
+            String text = "";
             if (currentNode != null) {
-                valueCount++;
-                pair = new Pair(currentNode.getValue().toString(), currentNode.getColor());
                 deque.add(currentNode.getLeftChild());
                 deque.add(currentNode.getRightChild());
+                if (currentNode.getColor() == Color.RED) {
+                    text = ANSI_RED + currentNode.getValue().toString() + ANSI_RESET;
+                }
+                else {
+                    text = currentNode.getValue().toString();
+                }
             }
             else {
                 deque.add(null);
                 deque.add(null);
             }
-            list.add(pair);
-            if (Math.pow(2, depth) == list.size() + 1)
-                run = false;
+            list.add(text);
+            run = (Math.pow(2, depth) != list.size() + 1);
         }
         return list;
     }
@@ -56,10 +69,12 @@ public class BinaryTreePrinter<T extends Comparable<T>> implements Printer{
      * @return
      */
     private int depth(Node<T> node, int currentDepth) {
-        if (node == null)
+        if (node == null) {
             return currentDepth;
-        if (node.getLeftChild() == null && node.getRightChild() == null)
+        }
+        if (node.getLeftChild() == null && node.getRightChild() == null) {
             return currentDepth + 1;
+        }
         return currentDepth + 1 +
                 Math.max(
                     depth(node.getLeftChild(), currentDepth),
@@ -67,52 +82,45 @@ public class BinaryTreePrinter<T extends Comparable<T>> implements Printer{
                 );
     }
 
-    @Override
-    public void print(Tree tree) {
-
+    private String getFormattedString(String text, int fieldsize) {
+        StringBuilder sb = new StringBuilder();
+        if (text == "") {
+            for (int i = 0; i < fieldsize; i++) {
+                sb.append(" ");
+            }
+            return sb.toString();
+        }
+        String txt = text
+                .replace(ANSI_RED,"")
+                .replace(ANSI_RESET,"");
+        if (fieldsize == txt.length() + 2) {
+            return " " + text + " ";
+        }
+        int spaceBefore = (fieldsize - txt.length()) / 2;
+        int spaceAfter = fieldsize - txt.length() - spaceBefore;
+        int spaceBefore1 = spaceBefore / 2;
+        int spaceBefore2 = spaceBefore - spaceBefore1 - 1;
+        int spaceAfter1 = spaceAfter / 2;
+        int spaceAfter2 = spaceAfter - spaceAfter1 - 1;
+        for (int i = 0; i < spaceBefore1; i++) {
+            sb.append(" ");
+        }
+        sb.append("┌");
+        for (int i = 0; i < spaceBefore2; i++) {
+            sb.append("-");
+        }
+        sb.append(text);
+        for (int i = 0; i < spaceAfter1; i++) {
+            sb.append("-");
+        }
+        sb.append("┐");
+        for (int i = 0; i < spaceAfter2; i++) {
+            sb.append(" ");
+        }
+        return sb.toString();
     }
 
-    private class Pair {
-
-        private String text;
-
-        private Color color;
-
-        public Pair() {}
-
-        public Pair(String text, Color color) {
-            this.text = text;
-            this.color = color;
-        }
-
-        public String getText() {
-            return text;
-        }
-
-        public Color getColor() {
-            return color;
-        }
-
-        public boolean isEmpty() {
-            return text == null && color == null;
-        }
-
-        @Override
-        public String toString() {
-            String out = "";
-            if (color == Color.RED)
-                out = ANSI_RED + text + ANSI_RESET;
-            else if (color == Color.BLACK)
-                out = ANSI_BLACK + text + ANSI_RESET;
-            else
-                out = text;
-            return out;
-        }
-
-        private String ANSI_RESET = "\u001B[0m";
-        private String ANSI_BLACK = "\u001B[30m";
-        private String ANSI_RED = "\u001B[31m";
-
-    }
+    private String ANSI_RESET = "\u001B[0m";
+    private String ANSI_RED = "\u001B[31m";
 
 }
--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,22 @@ public class Main {
 
         BinaryTree<Integer> myTree = new BinaryTree<>();
 
-//        for (int i = 0; i < 5; i++) {
-//            myTree.add(new Random().nextInt(100));
-//        }
+        for (int i = 0; i < 20; i++) {
+            myTree.add(new Random().nextInt(100));
+        }
 
 
-        myTree.add(5);
-        myTree.add(9);
-        myTree.add(8);
-        myTree.add(7);
-        myTree.add(6);
+//        myTree.add(15);
+//        myTree.add(9);
+//        myTree.add(8);
+//        myTree.add(17);
+//        myTree.add(6);
 
 
 
 //        myTree.printTree();
-        BinaryTreePrinter printer = new BinaryTreePrinter(myTree);
+        BinaryTreePrinter printer = new BinaryTreePrinter();
+        printer.print(myTree);
 
 
 
