e86624c8c142c46bf05adb8bdc53cfc63a7e912b
PeshkovDmitry <peshkov-dv@yandex.ru>
2023-08-06 23:48:22 +0300

    Улучшен вывод дерева на экран

--- src/BinaryTreePrinter.java
+++ src/BinaryTreePrinter.java
@@ -2,27 +2,120 @@ import java.util.*;
 
 public class BinaryTreePrinter<T extends Comparable<T>> implements Printer{
 
+    private List<String> redValues;
+
+    private List<String> blackValues;
+
+    public BinaryTreePrinter() {
+        redValues = new ArrayList<>();
+        blackValues = new ArrayList<>();
+    }
+
     @Override
     public void print(Tree tree) {
-        List<String> list = getFullTreeAsList(tree);
-        int maxValueLength = 0;
-        for (String s: list) {
-            if (s.replace(ANSI_RED,"").replace(ANSI_RESET,"").length() > maxValueLength) {
-                maxValueLength = s.replace(ANSI_RED,"").replace(ANSI_RESET,"").length();
+        List<String> list = getPlainList(tree);
+        int maxValueLength = getMaxLength(list) + 2;
+        int depth = getTreeDepth(tree.getRoot(),0);
+        List<String> lines = getTreeAsString(list, depth, maxValueLength);
+        cutHorizontal(lines);
+        markColor(lines);
+        for (String line: lines) {
+            System.out.println(line);
+        }
+    }
+
+    private void markColor(List<String> lines) {
+        String[] symbols = new String[] {" ", "┌", "┐", "-", "\n"};
+        for (int i = 0; i < lines.size(); i++) {
+            String currentLine = lines.get(i);
+            System.out.println(redValues);
+            for (String redValue: redValues) {
+                for (String startSym: symbols) {
+                    for (String endSym: symbols) {
+                        currentLine = currentLine.replace(startSym + redValue + endSym,
+                                startSym + ANSI_RED + redValue + ANSI_RESET + endSym);
+                    }
+                }
+                lines.set(i, currentLine);
             }
         }
-        maxValueLength += 2;
+    }
+
+    /**
+     * Метод, сжимающий дерево по горизонтали
+     * @param lines
+     */
+    private void cutHorizontal(List<String> lines) {
+        List<List<String>> newLines = new ArrayList<>();
+        for (int i = 0; i < lines.size() - 1; i++) {
+            newLines.add(new ArrayList<>());
+        }
+        for (int i = 0; i < lines.get(0).length(); i++) {
+            boolean canDelete = true;
+            for (int j = 0; j < lines.size() - 1; j++) {
+                canDelete = canDelete && !(lines.get(j).charAt(i) != ' ' && lines.get(j).charAt(i) != '-');
+            }
+            if (!canDelete) {
+                for (int j = 0; j < lines.size() - 1; j++) {
+                    newLines.get(j).add(lines.get(j).substring(i,i + 1));
+                }
+            }
+        }
+        for (int i = 0; i < newLines.size(); i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < newLines.get(i).size(); j++) {
+                if ((newLines.get(i).get(j).equals("┌") || newLines.get(i).get(j).equals("┐"))
+                        && (i + 1 == newLines.size() || newLines.get(i + 1).get(j).equals(" "))) {
+                    sb.append(" ");
+                }
+                else {
+                    sb.append(newLines.get(i).get(j));
+                }
+            }
+            lines.set(i, sb.toString());
+        }
+        lines.remove(newLines.size());
+    }
+
+    /**
+     * Метод, формирующий список строк с изображением дерева
+     * @param list
+     * @param depth
+     * @param maxValueLength
+     * @return
+     */
+
+    private List<String> getTreeAsString(List<String> list, int depth, int maxValueLength) {
         int level = 0;
         int nextNewLine = 0;
-        int depth = depth(tree.getRoot(),0);
+        List<String> lines = new ArrayList<>();
+        lines.add("");
         for (int i = 0; i < list.size(); i++) {
-            System.out.print(getFormattedString(list.get(i),(int) (maxValueLength * Math.pow(2, depth - level - 1))));
+            int currentLength = (int) (maxValueLength * Math.pow(2, depth - level - 1));
+            lines.set(level, lines.get(level) + getFormattedString(list.get(i),currentLength));
             if (i == nextNewLine) {
-                System.out.println();
                 level++;
+                lines.add("");
                 nextNewLine += Math.pow(2, level);
             }
         }
+        return lines;
+    }
+
+    /**
+     * Метод, возвращающий длину строки максимального значения
+     * @param list
+     * @return
+     */
+
+    private int getMaxLength(List<String> list) {
+        int maxValueLength = 0;
+        for (String s: list) {
+            if (s.length() > maxValueLength) {
+                maxValueLength = s.length();
+            }
+        }
+        return maxValueLength;
     }
 
     /**
@@ -33,23 +126,24 @@ public class BinaryTreePrinter<T extends Comparable<T>> implements Printer{
      * @return
      */
 
-    private List<String> getFullTreeAsList(Tree tree) {
+    private List<String> getPlainList(Tree tree) {
         List<String> list = new ArrayList<>();
         Deque<Node<T>> deque = new LinkedList<>();
         deque.add(tree.getRoot());
-        int depth = depth(tree.getRoot(),0);
+        int depth = getTreeDepth(tree.getRoot(),0);
         boolean run = true;
         while (deque.size() > 0 && run) {
             Node<T> currentNode = deque.pollFirst();
             String text = "";
             if (currentNode != null) {
+                text = currentNode.getValue().toString();
                 deque.add(currentNode.getLeftChild());
                 deque.add(currentNode.getRightChild());
                 if (currentNode.getColor() == Color.RED) {
-                    text = ANSI_RED + currentNode.getValue().toString() + ANSI_RESET;
+                    redValues.add(currentNode.getValue().toString());
                 }
-                else {
-                    text = currentNode.getValue().toString();
+                else if (currentNode.getColor() == Color.BLACK) {
+                    blackValues.add(currentNode.getValue().toString());
                 }
             }
             else {
@@ -68,7 +162,8 @@ public class BinaryTreePrinter<T extends Comparable<T>> implements Printer{
      * @param currentDepth
      * @return
      */
-    private int depth(Node<T> node, int currentDepth) {
+
+    private int getTreeDepth(Node<T> node, int currentDepth) {
         if (node == null) {
             return currentDepth;
         }
@@ -77,50 +172,51 @@ public class BinaryTreePrinter<T extends Comparable<T>> implements Printer{
         }
         return currentDepth + 1 +
                 Math.max(
-                    depth(node.getLeftChild(), currentDepth),
-                    depth(node.getRightChild(), currentDepth)
+                    getTreeDepth(node.getLeftChild(), currentDepth),
+                    getTreeDepth(node.getRightChild(), currentDepth)
                 );
     }
+
+    /**
+     * Метод, формирующий строку заданной длины и ветками
+     * @param text
+     * @param fieldsize
+     * @return
+     */
 
     private String getFormattedString(String text, int fieldsize) {
         StringBuilder sb = new StringBuilder();
         if (text == "") {
-            for (int i = 0; i < fieldsize; i++) {
-                sb.append(" ");
-            }
-            return sb.toString();
+            return " ".repeat(fieldsize);
         }
-        String txt = text
-                .replace(ANSI_RED,"")
-                .replace(ANSI_RESET,"");
-        if (fieldsize == txt.length() + 2) {
+        if (fieldsize == text.length() + 2) {
             return " " + text + " ";
         }
-        int spaceBefore = (fieldsize - txt.length()) / 2;
-        int spaceAfter = fieldsize - txt.length() - spaceBefore;
+        int spaceBefore = (fieldsize - text.length()) / 2;
+        int spaceAfter = fieldsize - text.length() - spaceBefore;
         int spaceBefore1 = spaceBefore / 2;
         int spaceBefore2 = spaceBefore - spaceBefore1 - 1;
         int spaceAfter1 = spaceAfter / 2;
         int spaceAfter2 = spaceAfter - spaceAfter1 - 1;
-        for (int i = 0; i < spaceBefore1; i++) {
-            sb.append(" ");
-        }
+        sb.append(" ".repeat(spaceBefore1));
         sb.append("┌");
-        for (int i = 0; i < spaceBefore2; i++) {
-            sb.append("-");
-        }
+        sb.append("-".repeat(spaceBefore2));
         sb.append(text);
-        for (int i = 0; i < spaceAfter1; i++) {
-            sb.append("-");
-        }
+        sb.append("-".repeat(spaceAfter1));
         sb.append("┐");
-        for (int i = 0; i < spaceAfter2; i++) {
-            sb.append(" ");
-        }
+        sb.append(" ".repeat(spaceAfter2));
         return sb.toString();
     }
 
+    /**
+     * Метод, возвращающий заданную строку, повторенную заданное число раз
+     * @param text
+     * @param count
+     * @return
+     */
+
     private String ANSI_RESET = "\u001B[0m";
+
     private String ANSI_RED = "\u001B[31m";
 
 }
